ca55856957b105295355ea76f662a77e05ba59c3
JoshuaVarner <110063334+JoshuaVarner@users.noreply.github.com>
2022-12-23 10:32:07 -0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -6,12 +6,5 @@ nc -l 80 <<EOF
 HTTP/1.1 200 OK
 Content-Type: text/html
 
-<html>
-<head>
-<title>My Simple Web Server</title>
-</head>
-<body>
-<h1>Welcome to my simple web server!</h1>
-</body>
-</html>
+$(cat index.html)
 EOF
